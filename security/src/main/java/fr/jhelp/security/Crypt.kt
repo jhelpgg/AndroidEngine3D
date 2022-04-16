a18e9652b9fe384fe5b04f2595c473c627ac23e3
jhelpgg <jhelpgg@gmail.com>
2022-04-16 09:05:47 +0100

    Automatic close management in encrypting

--- security/src/main/java/fr/jhelp/security/Crypt.kt
+++ security/src/main/java/fr/jhelp/security/Crypt.kt
@@ -25,10 +25,12 @@ import java.io.OutputStream
 fun <I : InputStream, O : OutputStream> encrypt(clear: () -> I, encrypted: () -> O,
                                                 error: (Exception) -> Unit = {
                                                     logError(it) { "Issue while encrypting!" }
-                                                }) =
+                                                },
+                                                automaticClose: Boolean = true) =
     treatInputOutputStream(clear, encrypted,
                            { input, output -> Security.encrypt(input, output) },
-                           error)
+                           error,
+                           automaticClose)
 
 /**
  * Helper for decrypt a stream.
@@ -42,7 +44,9 @@ fun <I : InputStream, O : OutputStream> encrypt(clear: () -> I, encrypted: () ->
 fun <I : InputStream, O : OutputStream> decrypt(encrypted: () -> I, clear: () -> O,
                                                 error: (Exception) -> Unit = {
                                                     logError(it) { "Issue while decrypting!" }
-                                                }) =
+                                                },
+                                                automaticClose: Boolean = true) =
     treatInputOutputStream(encrypted, clear,
                            { input, output -> Security.decrypt(input, output) },
-                           error)
+                           error,
+                           automaticClose)
